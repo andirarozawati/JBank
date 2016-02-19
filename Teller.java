a50a8165a3dad9b8f10eb20ffc0253270f734e63
andirarozawati <dumbxiu@gmail.com>
2016-02-20 00:49:22 +0700

    Modul 1

---
+++ Teller.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Teller here.
+ * 
+ * @author andira 
+ * @version version1
+ */
+public class Teller
+{
+    
+    public static void main(String args[])
+    {
+        // initialise instance variables
+        ;
+    }
+
+    public Teller ()
+    {
+        // put your code here
+        ;
+    }
+}
